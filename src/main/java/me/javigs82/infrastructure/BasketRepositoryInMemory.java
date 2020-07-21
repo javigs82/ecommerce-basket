6525ac522ef3961ef8a31517946ebcb60bfb5052
javigs82 <javigs82@gmail.com>
2020-07-21 20:41:09 +0200

    improve in memory structure for concurrent calls

--- src/main/java/me/javigs82/infrastructure/BasketRepositoryInMemory.java
+++ src/main/java/me/javigs82/infrastructure/BasketRepositoryInMemory.java
@@ -2,31 +2,37 @@ package me.javigs82.infrastructure;
 
 import me.javigs82.domain.Basket;
 import me.javigs82.domain.BasketRepository;
+import me.javigs82.domain.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
 public class BasketRepositoryInMemory implements BasketRepository {
 
     private final Logger log = LoggerFactory.getLogger(getClass().getName());
 
-    HashMap<String, Basket> baskets = new HashMap<>();
+    Map<String, Basket> baskets = new ConcurrentHashMap<>();
 
     public Optional<Basket> createBasket(String description) {
-        Basket basket = new Basket(description);
+        Basket basket = new Basket(UUID.randomUUID().toString(), description);
         this.baskets.put(basket.getCode(), basket);
-        log.info("memory size {}", this.baskets.size());
         return Optional.of(basket);
     }
 
     public Optional<Basket> deleteBasket(String code) {
+        log.debug("delete basket {}", code);
         if (this.baskets.containsKey(code)) {
+            log.debug("basket {} found", code);
             return Optional.of(this.baskets.remove(code));
         } else {
+            log.debug("basket {} NOT found", code);
             return Optional.empty();
         }
     }
@@ -39,9 +45,13 @@ public class BasketRepositoryInMemory implements BasketRepository {
         }
     }
 
-    public Basket addItemToBasket(String code, String itemCode) {
-        return this.baskets.get(code);
+    public Optional<Basket> addItemToBasket(String code, Item item) {
+        if (this.baskets.containsKey(code)) {
+            Basket basket = this.baskets.get(code);
+            basket.addItem(item);
+            return Optional.of(basket);
+        } else {
+            return Optional.empty();
+        }
     }
-
-
 }
